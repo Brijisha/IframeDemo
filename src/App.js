2ed01041abd60d228fd3486c297aa232d0885322
Brijisha <brijishadoshi12@gmail.com>
2023-03-02 14:49:10 +0530

    Completed iframe demo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Iframe from './Iframe';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Here is demo of Iframe. </h1>
+      <Iframe src="https://test.skillsquirrel.com" width="60vw" height="60vh"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Iframe.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from 'react';
+
+function Iframe(props) {
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    const iframe = document.createElement('iframe');
+    iframe.src = props.src;
+    iframe.style.width = props.width || '100%';
+    iframe.style.height = props.height || '100%';
+    iframe.style.border = 'none';
+
+    iframeRef.current.appendChild(iframe);
+
+    return () => {
+      iframeRef.current.removeChild(iframe);
+    };
+  }, [props.src, props.width, props.height]);
+
+  return <div ref={iframeRef}></div>;
+}
+
+export default Iframe;
+
+
