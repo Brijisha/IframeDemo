2d7af0f732ae2656111f08801a47d6e3174e3ee3
Brijisha <brijishadoshi12@gmail.com>
2023-03-02 15:43:22 +0530

    Add coppy button beside the  textarea.

--- src/EmbedButton.js
+++ src/EmbedButton.js
@@ -6,8 +6,13 @@ function EmbedButton() {
   const togglePopup = () => {
     setShowPopup(!showPopup);
   }
+ 
 
   const iframeCode = '<iframe src="https://test.skillsquirrel.com" width="60vw" height="60vh" border="solid 2px"></iframe>';
+ 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(iframeCode);
+  }
 
   return (
     <div style={{margin:20}}>
@@ -15,7 +20,10 @@ function EmbedButton() {
       {showPopup && (
         <div className="popup">
           <p style={{fontFamily:'sans-serif', fontStyle:'oblique', fontSize:"1vw"}}>Copy the following iframe code to embed the website in your page:</p>
+          <div className="code-wrapper">
           <textarea style={{fontFamily:'sans-serif', width:"80%", width:"80%"}} value={iframeCode} readOnly />
+           <button onClick={handleCopy} className="copy-icon">&#x2398;</button>
+          </div>
           <button onClick={togglePopup}>Close</button>
         </div>
       )}
